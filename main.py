245455c57e8ee5ddc6fda1bd7b8ca70d5a728085
YellowCytrus <lancasterproxima@ya.ru>
2025-10-15 15:47:39 +0700

    Merge pass-gen branch with updated main.py and new pass-gen file

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import os
+import random
+import string
 
 print("Hello world!")
 
+def generate_password(length: int):
+	return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+
+print(generate_password(10))
+
+
+def generate_krypton_password(length: int):
+	return "".join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
+
+print(generate_krypton_password(10))
