7f137257a19013ca06b8cff945f58881233241dc
YellowCytrus <lancasterproxima@ya.ru>
2025-10-15 15:16:47 +0700

    pass generators tests

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 def some_test():
 	return
+
+def test_for_password():
+	assert some_test() == "1234567890"
+
+def test_for_password_length():
+	assert len(some_test()) == 10
+
+def test_for_password_type():
+	assert isinstance(some_test(), str)
+
+def test_for_password_complexity():
+	assert len(some_test()) >= 10
